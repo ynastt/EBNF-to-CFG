5d1e9b946cb77be6d3c269b290a5b675648b40e5
ynastt <yarovikovaanastasia1@gmail.com>
2023-01-06 02:22:08 +0400

    Anastasia forgot all the pluskaz of cpp syntax omg so sorry god damn

--- main.cpp
+++ main.cpp
@@ -16,7 +16,8 @@ map <string, string> parametrs {
     {"TermStart", ""},
     {"TermEnd", ""},
     {"NtermStart", "'"},
-    {"NtermEnd", "'"}
+    {"NtermEnd", "'"},
+    {"Empty", "#"}
     // {"Operation[]Start", "["},
     // {"Operation[]End", "]"},
     // {"Operation{}Start", "{"},
@@ -32,7 +33,9 @@ map <string, string> parametrsCFG {
     {"TermStart", ""},
     {"TermEnd", ""},
     {"NtermStart", "'"},
-    {"NtermEnd", "'"}
+    {"NtermEnd", "'"},
+    {"Concat",""},
+    {"Alternative","|"}
 };
 
 // множество используемых обозначений операций
@@ -62,15 +65,15 @@ struct Rule {
 // соотвественно vector[0] - первое правило грамматики, правило переписывания нетерминла S
 vector<Rule*> Grammar;
 
-string Clear_str(string str){
+string Clear_str(string str) {
     string v;
     for(char c : str) if (c != ' ') v += c;
     return v;
 }
 
-void inputSyntax(int n){
+void inputSyntax(int n) {
     string testName = "tests\\test" + to_string(n) + "\\syntax.txt";
-    cout << testName << endl;
+    // cout << testName << endl;
     ifstream file(testName);
     if (file.is_open()) {
         string str;
@@ -94,32 +97,6 @@ void inputSyntax(int n){
     }
 }
 
-void inputCFGsyntax(int n){
-    string testName = "tests\\test" + to_string(n) + "\\CFGsyntax.txt";
-    cout << testName << endl;
-    ifstream file(testName);
-    if (file.is_open()) {
-        string str;
-        while (getline(file,str)) {
-            if (str.size()) {
-                int pos = str.find("=");
-                for(auto mypair: parametrsCFG) {
-                    string key = mypair.first;
-                    if (str.substr(0, pos) == key) {
-                        if (str.substr(pos+2) != "") {
-                            parametrsCFG[key] = str.substr(pos+2);
-                            // cout << key << " - " << parametrsCFG[key] << endl;
-                        }
-                    }
-                }
-            }
-        }
-        file.close();
-    } else {
-        cout << "file 'syntax.txt' does not exist. All parametrs are default" << endl;
-    }
-}
-
 // функция находит индекс первой внешней альтернативы
 // если -1 - правило не имеет вид альтернативы, значит это конкатенация чего-то
 // если не -1, то дан индекс первой альтенативы 
@@ -128,9 +105,11 @@ int getFirstAltIndex (string str) {
     int outBracketsCount1 = 0;
     int outBracketsCount2 = 0;
     int outBracketsCount3 = 0;
-	for (int i =0; i < str.size(); i++) {
-		if (outBracketsCount1 == outBracketsCount2 == outBracketsCount3 == 0 && 
-		str[i] == '|') {
+	for (int i = 0; i < str.size(); i++) {
+        //printf("counters are\n(): %d\n{}: %d\n[]: %d\n", outBracketsCount1, outBracketsCount2, outBracketsCount3);
+        //cout << "letter: " << str[i] << endl;
+		if (outBracketsCount1 == 0 && outBracketsCount2 == 0 &&
+             outBracketsCount3 == 0 && str[i] == '|') {
 			return i;
 		} else if (str[i] == '(') {
 			outBracketsCount1++;
@@ -144,19 +123,34 @@ int getFirstAltIndex (string str) {
 			outBracketsCount3++;
 		} else if (str[i] == ']') {
 			outBracketsCount3--;
-		}
+		} else {
+            continue;
+        }
 	}
 	return -1;
+}
+
+int findClosingParanthesis( string str, char pstart, char pend) {
+    int c = 0;
+	for (int i = 0; i < str.size(); i++) {
+		if (str[i] == pstart) {
+			c++;
+		} else if (str[i] == pend) {
+			c--;
+			if (c == 0) {
+				return i;
+			}
+		}
+	}
+    return -1;
 }
 
 struct Tree* parseAlt(string str);
 
 struct Tree* parseConcat(string str) {
-    cout << "CONC: " << str << endl;
     struct Tree* tr = new Tree;
     if (str.length() == 1) {
         if (islower(str[0])) {
-            cout << "LOWWER" << endl;
             tr->str = str;
             tr->num = 5;
             tr->left = NULL;
@@ -164,14 +158,13 @@ struct Tree* parseConcat(string str) {
             return tr;
         }
         if (isupper(str[0])) {
-            cout << "UPPER" << endl;
             tr->str = str;
             tr->num = 6;
             tr->left = NULL;
             tr->right = NULL;
             return tr;
         }
-        if (str == EMPTY) {
+        if (str == parametrs["Empty"]/*EMPTY*/) {
             tr->str = str;
             tr->num = 7;
             tr->left = NULL;
@@ -196,7 +189,7 @@ struct Tree* parseConcat(string str) {
             tr->right = NULL;
             return tr;
         }
-        if (str[0] == '(' && str[2] == ')') {   // такого случая наверное не будет в норм грамматике пользователя
+        if (str[0] == '(' && str[2] == ')') {   
             string s = {str[1]};
             tr->str = str;
             tr->num = 2;
@@ -215,66 +208,69 @@ struct Tree* parseConcat(string str) {
         return tr;
     }
     if (str[0] == '[') {
-        int pos = str.rfind(']');
-        if (pos != str.size() - 1) {
-            tr->str = str;
-            tr->num = 4;
-            tr->left = parseAlt(str.substr(0, pos + 1));
-            tr->right = parseConcat(str.substr(pos + 1));
-            return tr;
-        } else if (pos == str.size() - 1){
-            cout << "HERE []" << endl;
-            tr->str = str;
-            tr->num = 0;
-            tr->left = parseAlt(str.substr(1,pos - 1));
-            tr->right = NULL;
-            return tr;
+        int pos = findClosingParanthesis(str, '[', ']');
+        if (pos != -1) {
+            if (pos != str.size() - 1) {
+                tr->str = str;
+                tr->num = 4;
+                tr->left = parseAlt(str.substr(0, pos + 1));
+                tr->right = parseConcat(str.substr(pos + 1));
+                return tr;
+            } else if (pos == str.size() - 1) {
+                tr->str = str;
+                tr->num = 0;
+                tr->left = parseAlt(str.substr(1,pos - 1));
+                tr->right = NULL;
+                return tr;
+            }
         }    
     }
     if (str[0] == '{') {
-        // не просто первую закрывающую, а последнюю
-        int pos = str.rfind('}');
-        if (pos < str.size() - 1) {
-            cout << "HERE 1 {}" << endl;
-            tr->str = str;
-            tr->num = 4;
-            tr->left = parseAlt(str.substr(0, pos + 1));
-            tr->right = parseConcat(str.substr(pos + 1));
-            return tr;
-        } else if (pos == str.size() - 1){
-            cout << "HERE {}" << endl;
-            tr->str = str;
-            tr->num = 1;
-            tr->left = parseAlt(str.substr(1, pos - 1));
-            tr->right = NULL;
-            return tr;
+        // здесь поиск закрывающей соответствующей скобки
+        int pos = findClosingParanthesis(str, '{', '}');
+        if (pos != -1) {
+            if (pos < str.size() - 1) {
+                tr->str = str;
+                tr->num = 4;
+                tr->left = parseAlt(str.substr(0, pos + 1));
+                tr->right = parseConcat(str.substr(pos + 1));
+                return tr;
+            } else if (pos == str.size() - 1) {
+                tr->str = str;
+                tr->num = 1;
+                tr->left = parseAlt(str.substr(1, pos - 1));
+                tr->right = NULL;
+                return tr;
+            }
         }    
     }
     if (str[0] == '(') {
-        int pos = str.rfind(')');
-        if (pos != str.size() - 1) {
-            tr->str = str;
-            tr->num = 4;
-            tr->left = parseAlt(str.substr(0, pos + 1));
-            tr->right = parseConcat(str.substr(pos + 1));
-            return tr;
-        } else {
-            tr->str = str;
-            tr->num = 2;
-            tr->left = parseAlt(str.substr(1,pos - 1));
-            tr->right = NULL;
-            return tr;
-        }    
+        int pos = findClosingParanthesis(str, '(', ')');
+        if (pos != -1) {
+            if (pos != str.size() - 1) {
+                tr->str = str;
+                tr->num = 4;
+                tr->left = parseAlt(str.substr(0, pos + 1));
+                tr->right = parseConcat(str.substr(pos + 1));
+                return tr;
+            } else {
+                tr->str = str;
+                tr->num = 2;
+                tr->left = parseAlt(str.substr(1,pos - 1));
+                tr->right = NULL;
+                return tr;
+            }    
+        }
     }
     return tr;
 }
 
 struct Tree* parseAlt(string str) {
-    cout << "ALT: " << str << endl;
+    //cout << "ALT: " << str << endl;
     struct Tree* t = new Tree;
     int alt = getFirstAltIndex(str);
+    //cout << "first outter alt: " << alt << endl;
     if (alt == -1) {
-        cout << "debug - concat root" << endl;
         t = parseConcat(str);  
 	} else {
         t->str = str;
@@ -390,8 +386,11 @@ bool inputGrammar(int n) {
     else return (false);
 }
 
-void printTree(struct Tree* t) {    
-    cout << t -> str << endl;;
+void printTree(struct Tree* t) {  
+    //if (t != NULL) {
+        cout << t->str << " " << t->num << endl;
+    //}  
+    
     if (t->left != NULL) {
         printTree(t->left); 
     } 
@@ -416,27 +415,215 @@ void printGrammar() {
     
 }
 
+struct cfg_rule {
+    string leftpart;
+    string rightpart;
+};
+
+vector <cfg_rule> cfg_answer;
+
+vector <cfg_rule> onerule;
+ 
+vector <string> forcheck;
+
+//Функция для генерации разных нетерминалов
+string GeneratorNewNeterm(){
+    int num=0;
+    string newneterm="A";
+    while (true){
+        int flag=0;
+        for (int i=0;i<forcheck.size();i++){
+            if (forcheck[i].find(to_string(num))!=-1){
+                flag=1;
+                break;
+            }
+        }
+        if (!flag) break;
+        else num++;
+    }
+    return newneterm+to_string(num);
+}
+
+/*
+map <string, string> parametrsCFG {
+    {"Delim", "\n"},
+    {"Arrow", "->"},
+    {"Empty", ""},
+    {"TermStart", ""},
+    {"TermEnd", ""},
+    {"NtermStart", "'"},
+    {"NtermEnd", "'"}
+};
+*/
+// [] - 0; {} - 1; () - 2; | - 3; concat - 4; a-z -5; A-Z -6, empty - 7;
+string ConvertertoCFG_1(string leftpart,struct Tree* p,int flag){
+    string str;
+   if (p!=NULL){
+    if (p->num==4){
+        if ((p->left!=NULL)&&(p->right!=NULL))str=ConvertertoCFG_1(leftpart,p->left,0)+parametrsCFG["Concat"]+ConvertertoCFG_1(leftpart,p->right,0);
+    }
+    else if (p->num==5){
+        str=parametrsCFG["TermStart"]+p->str+parametrsCFG["TermEnd"];
+    }
+    else if (p->num==6){
+        str=parametrsCFG["NtermStart"]+p->str+parametrsCFG["NtermEnd"];
+    }
+    else if (p->num==7){
+        str=parametrsCFG["Empty"];
+    }
+    else if ((p->num==3)&&(!flag)){
+        string newneterm=GeneratorNewNeterm();
+        str=parametrsCFG["NtermStart"]+newneterm+parametrsCFG["NtermEnd"];
+        forcheck.push_back(newneterm);
+        ConvertertoCFG_1(newneterm,p,1);
+        //str=ConvertertoCFG_1(newneterm,p->left)+"|"+ConvertertoCFG_1(newneterm,p->right);
+    }
+    else if ((p->num==3)&&(flag)){
+        if ((p->left!=NULL)&&(p->right!=NULL)) str=ConvertertoCFG_1(leftpart,p->left,0)+parametrsCFG["Alternative"]+ConvertertoCFG_1(leftpart,p->right,0);
+    }
+    else if ((p->num==0)&&(!flag)){
+        string newneterm=GeneratorNewNeterm();
+        str=parametrsCFG["NtermStart"]+newneterm+parametrsCFG["NtermEnd"];
+        forcheck.push_back(newneterm);
+        ConvertertoCFG_1(newneterm,p,1);
+    }
+    else if ((p->num==0)&&(flag)){
+        if ((p->left!=NULL)) str=ConvertertoCFG_1(leftpart,p->left,0)+parametrsCFG["Alternative"]+parametrsCFG["Empty"];
+    }
+    else if ((p->num==1)&&(!flag)){
+        string newneterm=GeneratorNewNeterm();
+        str=parametrsCFG["NtermStart"]+newneterm+parametrsCFG["NtermEnd"];
+        forcheck.push_back(newneterm);
+        ConvertertoCFG_1(newneterm,p,1);
+        //str=ConvertertoCFG_1(newneterm,p->left)+"|"+ConvertertoCFG_1(newneterm,p->right);
+    }
+    else if ((p->num==1)&&(flag)){
+        if ((p->left!=NULL)) str=ConvertertoCFG_1(leftpart,p->left,0)+parametrsCFG["NtermStart"]+leftpart+parametrsCFG["NtermEnd"]+parametrsCFG["Alternative"]+parametrsCFG["Empty"];
+    }
+    else if ((p->num==2)){
+        if ((p->left!=NULL)) str=ConvertertoCFG_1(leftpart,p->left,0);
+    }
+   }
+    if (onerule.empty()){
+        struct cfg_rule dop;
+        dop.leftpart=leftpart;
+        dop.rightpart=str;
+        onerule.push_back(dop);
+    }
+    else{
+        int find=0;
+        for (int i=0;i<onerule.size();i++){
+            if (onerule[i].leftpart==leftpart){
+                onerule[i].rightpart=str;
+                find=1;
+            }
+        }
+        if (!find){
+            struct cfg_rule dop;
+            dop.leftpart=leftpart;
+            dop.rightpart=str;
+            onerule.push_back(dop);
+        }
+    }
+    //cout<<leftpart<<"\n";
+    return str;
+} 
+
+//Чекаем правую сторону, 
+//vector <cfg_rule> cfg_answer;
+
+void NormOrder(){
+    for (int i=0;i<onerule.size();i++){
+        if (onerule[i].leftpart.size()==1){
+            onerule[i].leftpart=parametrsCFG["NtermStart"]+onerule[i].leftpart+parametrsCFG["NtermEnd"];
+            cfg_answer.push_back(onerule[i]);
+            break;
+        }
+    }
+    string neterm="A";
+    int num=0;
+    string str;
+    str=neterm+to_string(num);
+    if (onerule.size()>1){
+        while (true){
+        int flag=0;
+        for (int i=0;i<forcheck.size();i++){
+            if (forcheck[i]==str){
+                flag=1;
+                break;
+            }
+        }
+        if (!flag) break;
+        for (int i=0;i<onerule.size();i++){
+            if (onerule[i].leftpart==str){
+                onerule[i].leftpart=parametrsCFG["NtermStart"]+onerule[i].leftpart+parametrsCFG["NtermEnd"];
+                cfg_answer.push_back(onerule[i]);
+                break;
+            }
+        }
+        num++;
+        str=neterm+to_string(num);
+        }
+    }
+    
+}
+
+void ConvertertoCFG(){
+    for (int i=0;i<Grammar.size();i++){
+        onerule.clear();
+        ConvertertoCFG_1(Grammar[i]->left,Grammar[i]->right,0);
+        if (!onerule.empty()) NormOrder();
+    }
+}
+
+void PrintAnswer(int n){
+    ofstream f1;
+    f1.open("tests\\test" + to_string(n) + "\\output.txt");
+    for (int i=0;i<cfg_answer.size();i++){
+        string str=cfg_answer[i].leftpart+parametrsCFG["Arrow"]+cfg_answer[i].rightpart+parametrsCFG["Delim"];
+        cout<<cfg_answer[i].leftpart<<parametrsCFG["Arrow"]+cfg_answer[i].rightpart<<parametrsCFG["Delim"];
+        f1<<str;
+    }
+}
+
+void inputSyntax_2(int n) {
+    string testName = "tests\\test" + to_string(n) + "\\CFGsyntax.txt";
+    // cout << testName << endl;
+    ifstream file(testName);
+    if (file.is_open()) {
+        string str;
+        while (getline(file,str)) {
+            if (str.size()) {
+                int pos = str.find("=");
+                for(auto mypair: parametrsCFG) {
+                    string key = mypair.first;
+                    if (str.substr(0, pos) == key) {
+                        if (str.substr(pos+2) != "") {
+                            parametrsCFG[key] = str.substr(pos+2);
+                            // cout << key << " - " << parametrs[key] << endl;
+                        }
+                    }
+                }
+            }
+        }
+        file.close();
+    } else {
+        cout << "\nfile 'CFGsyntax.txt' does not exist. All parametrs are default" << endl;
+    }
+}
+
+
+
 int main() {
     int n;
     cout << "Enter test number" << endl;
     cin >> n;
     inputSyntax(n);
-
     cout << ">> PARAMETRS <<" << endl;
     for (auto mypair: parametrs) {  
         cout << mypair.first << " = " << mypair.second << endl;
     } 
 
-    // добавляем параметры для КС 
-    //inputCFGsyntax(n);
-    // cout << ">> CFG PARAMETRS <<" << endl;
-    // for (auto mypair: parametrsCFG) {  
-    //     cout << mypair.first << " = " << mypair.second << endl;    
-    // } 
-
-    // парсим грамматику
-    // если в грамматике не использованы значения по умолчанию (а параметры не были заполнены), 
-    // то говорим об ошибке err
     bool empty;
     empty  = inputGrammar(n);
     if (empty) {
@@ -446,13 +633,18 @@ int main() {
         cout << "\nGrammar is fine! " << endl;
     }
 
+    inputSyntax_2(n);
+    cout << ">> PARAMETRS FOR CFG <<" << endl;
+    for (auto mypair: parametrsCFG) {  
+        cout << mypair.first << " = " << mypair.second << endl;
+    } 
+
     cout << ">> PARSED GRAMMAR <<" << endl;
     printGrammar();
+    cout<<"\n\n";
 
-
-    // TO DO
-    // конвертируем в КС (папраметры Кс грамматики считываются выше)
-    
-    // выводим КС
+    ConvertertoCFG();
+    cout<<"\n.....CFG.....\n";
+    PrintAnswer(n);
     return 0;
 }
